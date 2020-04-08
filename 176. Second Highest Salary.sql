3a8372ca1f3a2d38c12e7d4525b708751f7be357
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-08 19:45:33 -0400

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -1,1 +1,23 @@
-select 
+#My Submission (Using NOT IN Operator)
+
+select max(Salary) as SecondHighestSalary
+from Employee
+where Salary not in (Select max(Salary) from Employee);
+
+#Other Solutions (Using Sub Query and < Operator)
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (select max(salary) from Employee)
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+    
+
+#The IFNULL() function returns a specified value if the expression is NULL. 
+#select 100 as sum;
