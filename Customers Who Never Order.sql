c6166c399ec52a49312c74355fa15214f17a43c8
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-08 21:56:41 -0400

    Retry. Incorrect result. Doubt

---
+++ Customers Who Never Order.sql
@@ -0,0 +1,10 @@
+My Solution:
+
+select c.Name as 'Customers' from Customers c
+join Orders o 
+on c.Id = o.CustomerId and c.Id not in (
+    select customerid from orders
+);
+
+This is an incorrect query. Retry it again. 
+
