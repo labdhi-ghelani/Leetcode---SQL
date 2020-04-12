8bfd962ec695afdec29f51fe8acdc6a58ebc3142
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-12 08:47:52 -0400

    MIN() and MAX() with dates, GROUPBY, HAVING

---
+++ 1084. Sales Analysis III.sql
@@ -0,0 +1,17 @@
+SELECT product_id, product_name
+FROM Sales 
+JOIN Product 
+Using(product_id)
+GROUP BY product_id
+HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31' 
+
+The MIN(), MAX() logic is crucial here. It will help to sepearate S8 and G4. Without using min() and max() if you try it 
+with simple where clause you will get both S8 and G4. hence since you have to use min and max with the group by and having clause.
+
+Another great solution,
+
+SELECT Product.product_id, Product.product_name
+FROM Product
+WHERE product_id NOT IN (SELECT product_id FROM Sales WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31');
+
+The second solution is intuitive, smart and fast. 
