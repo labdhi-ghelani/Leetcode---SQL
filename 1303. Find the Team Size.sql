e4b777255f3b6e400bbde6ce178493373de15fad
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-15 22:36:15 -0400

    WINDOW function, JOIN, SUBQUERY

---
+++ 1303. Find the Team Size.sql
@@ -0,0 +1,13 @@
+My solution:
+
+select employee_id, count(employee_id) over (partition by team_id) as team_size
+from Employee1
+order by employee_id;
+
+Although it generates a syntax error on leetcode but works fine on sql workbench (O_o)
+
+Other smart solution with join and subquery:
+
+SELECT e.employee_id, t.team_size FROM Employee e JOIN
+(SELECT team_id, COUNT(DISTINCT employee_id) as team_size FROM Employee GROUP BY team_id) t
+ON t.team_id = e.team_id
