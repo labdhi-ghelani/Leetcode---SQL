e63a81d6cd15fba7ef55567cdbb4e97bc7e4b761
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-17 02:13:14 -0400

    is null(), coalesce()

--- 577. Employee Bonus.sql
+++ 577. Employee Bonus.sql
@@ -1,3 +1,17 @@
 select e.name, b.bonus from Employee e
 left join Bonus b on e.empID = b.empID 
 where b.bonus < 1000 or b.bonus is null;
+
+
+Other solution:
+with COALESCE
+SELECT
+e.name,
+b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b. empId
+WHERE COALESCE(b.bonus, 0) < 1000
+
+COALESCE
+https://www.w3schools.com/sql/func_sqlserver_coalesce.asp
