e243339235e9d0b16698f2145c5722b883dd94f6
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-25 04:12:48 -0400

    UNION ALL

--- Medium/602. Friend Requests II: Who Has the Most Friends.sql
+++ Medium/602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,1 +1,30 @@
+My solution: (Totally wrong and horrible)
 
+select requester_id, sum(count(requester_id)+count(accepter_id)) as numcount
+from
+(
+select requester_id, count(requester_id)
+from request_accepted
+group by requester_id) as a
+left join 
+(select accepter_id, count(accepter_id)
+from request_accepted
+group by accepter_id
+) as b
+on a.requester_id = b.accepter_id
+group by requester_id
+;
+                                                   
+#Cannot use sum(Count())
+                
+Other smart solution:
+                
+SELECT ids as id , COUNT(*) as num
+FROM 
+  (SELECT requester_id as ids FROM request_accepted
+   UNION ALL
+   SELECT accepter_id as ids FROM request_accepted) AS u
+GROUP BY ids
+ORDER BY COUNT(*) DESC
+LIMIT 1             
+                                                   
