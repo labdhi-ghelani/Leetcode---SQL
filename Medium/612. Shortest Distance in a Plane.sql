2b2d8fefd6f65c33d8b3a3cd8f309200aa92130e
labdhi-ghelani <44735660+labdhi-ghelani@users.noreply.github.com>
2020-04-30 08:17:30 -0400

    Using SQRT, POW() functions and math knowledge , DIY

---
+++ Medium/612. Shortest Distance in a Plane.sql
@@ -0,0 +1,10 @@
+SELECT
+    ROUND(SQRT(MIN((POW(p1.x - p2.x, 2) + POW(p1.y - p2.y, 2)))), 2) AS shortest
+FROM
+    point_2d p1
+        JOIN
+    point_2d p2 ON p1.x != p2.x OR p1.y != p2.y
+;
+
+Try it on your own !!!
+https://leetcode.com/problems/shortest-distance-in-a-plane/solution/
